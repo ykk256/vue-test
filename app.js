eb50d5c09a7bfcaf09bc9910a46aa495ef5a7056
ykk256 <odruahou@hotmail.co.jp>
2019-06-08 18:24:55 +0900

    over value

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var items = [
     {
         name:'鉛筆',
-        price:300 ,
+        price:1000 ,
         quantity:1
     },
     {
         name:'ノート',
         price:400,
-        quantity:1
+        quantity:0
     },
     {
         name:'消しゴム',
         price:500,
-        quantity:1
+        quantity:0
     }
 ]
 
@@ -40,7 +40,10 @@ var vm = new Vue({
     	totalPriceWithTax: function() {
     		//算出プロパティに依存した算出プロパティも定義できる。
     		return Math.floor(this.totalPrice * 1.08)
-    	}
+        },
+        canBuy: function() {
+            return this.totalPrice >= 1000
+        }
     }
 })
 window.vm = vm
